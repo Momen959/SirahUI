bff83bcad88aca93387ecab4f4ee286687823d43
Momen Saif <momen.saif22@gmail.com>
2025-07-31 21:19:39 +0000

    okay I don't mean saved responses, there is a book logo that always acco

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -14,9 +14,9 @@ export function SirahSenseLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20" />
-      <path d="m9 9 2 2 2-2" />
-      <path d="m11 15-2-2 2-2" />
+        <path d="M12 2L2 7l10 5 10-5-10-5z" />
+        <path d="M2 17l10 5 10-5" />
+        <path d="M2 12l10 5 10-5" />
     </svg>
   );
 }
